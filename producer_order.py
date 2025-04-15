6e8f76d7696ca11afda2ff97c11732e495950a56
LOKESH TALLURI <97298610+Lokesh-T-2506@users.noreply.github.com>
2025-04-14 20:49:16 -0400

    Update producer_order.py

--- producer_order.py
+++ producer_order.py
@@ -7,12 +7,14 @@ from rabbitmq_config import RABBITMQ_HOST, EXCHANGE_NAME
 def generate_random_order():
     order_id = str(uuid.uuid4())[:8] 
     user_id = f"u{random.randint(100, 999)}" 
-    book_id = f"b{random.randint(1000, 9999)}" 
+    book_id = f"b{random.randint(1000, 9999)}"
+    student_name = "Lokesh Talluri" 
 
     return {
         "order_id": order_id,
         "user_id": user_id,
-        "book_id": book_id
+        "book_id": book_id,
+        "student_name": student_name
     }
 
 def place_order():
@@ -24,7 +26,7 @@ def place_order():
     order = generate_random_order()
 
     channel.basic_publish(exchange=EXCHANGE_NAME, routing_key="order-created", body=json.dumps(order))
-    print(f"Order placed: {order}")
+    print(f"[{order['student_name']}] Producer: Order placed: {order}")
 
     connection.close()
 
